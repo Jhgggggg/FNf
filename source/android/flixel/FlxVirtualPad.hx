b3d84287e9498a4277088a8340294aa7bd921b8f
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-07-06 07:19:29 +0800

    Update FlxVirtualPad.hx

--- source/android/flixel/FlxVirtualPad.hx
+++ source/android/flixel/FlxVirtualPad.hx
@@ -114,6 +114,9 @@ class FlxVirtualPad extends FlxSpriteGroup {
 			case A_B:
 				actions.add(add(buttonA = createButton(FlxG.width - 44 * 3, FlxG.height - 45 * 3, 44 * 3, 45 * 3, "a")));
 				actions.add(add(buttonB = createButton(FlxG.width - 86 * 3, FlxG.height - 45 * 3, 44 * 3, 45 * 3, "b")));
+			case A_B_alt:
+				actions.add(add(buttonB = createButton(FlxG.width - 44 * 3, FlxG.height - 45 * 3, 44 * 3, 45 * 3, "b")));
+				actions.add(add(buttonA = createButton(FlxG.width - 86 * 3, FlxG.height - 45 * 3, 44 * 3, 45 * 3, "a")));
 			case A_B_C:
 				actions.add(add(buttonC = createButton(FlxG.width - 128 * 3, FlxG.height - 45 * 3, 44 * 3, 45 * 3, "c")));
 				actions.add(add(buttonB = createButton(FlxG.width - 86 * 3, FlxG.height - 45 * 3, 44 * 3, 45 * 3, "b")));								
@@ -242,6 +245,7 @@ enum FlxActionMode {
 	B;
 	D;
 	A_B;
+	A_B_alt;
 	A_B_C;
 	A_B_E;
 	A_B_X_Y;
