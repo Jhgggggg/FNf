6fb9e91e948fd799d443f22d3e4eb13f111947e4
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2023-07-12 16:56:04 +0800

    Update StoryMenuState.hx
    (why doesnt the web editor work aaaaaa)

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -185,9 +185,9 @@ class StoryMenuState extends MusicBeatState
 		changeWeek();
 		changeDifficulty();
 
-        #if android
-        addVirtualPad(FULL, A_B_C);
-        #end
+                #if android
+                addVirtualPad(FULL, A_B_X_Y);
+                #end
 
 		super.create();
 	}
@@ -248,12 +248,12 @@ class StoryMenuState extends MusicBeatState
 			else if (upP || downP)
 				changeDifficulty();
 
-			if(FlxG.keys.justPressed.CONTROL)
+			if(FlxG.keys.justPressed.CONTROL #if android || _virtualpad.buttonX.justPressed #end)
 			{
 				persistentUpdate = false;
 				openSubState(new GameplayChangersSubstate());
 			}
-			else if(controls.RESET)
+			else if(controls.RESET #if android || _virtualpad.buttonY.justPressed #end)
 			{
 				persistentUpdate = false;
 				openSubState(new ResetScoreSubState('', curDifficulty, '', curWeek));
