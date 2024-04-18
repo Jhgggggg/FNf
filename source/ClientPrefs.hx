dd218b3b315b21beb66381b8f28198db862a3daa
Jhgggggg <134745520+Jhgggggg@users.noreply.github.com>
2024-04-17 21:25:47 -0300

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -7,7 +7,7 @@ import flixel.graphics.FlxGraphic;
 import Controls;
 
 class ClientPrefs {
-	public static var downScroll:Bool = false;
+	public static var downScroll:Bool = true;
 	public static var middleScroll:Bool = false;
 	public static var opponentStrums:Bool = true;
 	public static var showFPS:Bool = true;
@@ -22,7 +22,7 @@ class ClientPrefs {
 	public static var camZooms:Bool = true;
 	public static var camMovement:Bool = true;
 	public static var hideHud:Bool = false;
-	public static var noteOffset:Int = 0;
+	public static var noteOffset:Int = 5;
 	public static var arrowHSV:Array<Array<Int>> = [[0, 0, 0], [0, 0, 0], 
 													[0, 0, 0], [0, 0, 0], 
 													[0, 0, 0], [0, 0, 0], 
